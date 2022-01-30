b50ad63cd511a6118ed261ec2625bb6bd27e64c6
caio-vinicius <contato@caiovinicius.dev.br>
2022-01-30 11:14:49 -0300

    add GlobalBankstersUnited files saved on usb

---
+++ 02/Account.cpp
@@ -0,0 +1,23 @@
+/* Copyright (c) 2022 Caio Souza. All rights reserved. */
+/* 42 */
+
+#include "Account.hpp"
+
+Account::Account(int initial_deposit) {}
+Account::~Account(void) {}
+
+void Account::displayAccountsInfos(void) {
+    return ;
+}
+
+void Account::displayStatus(void) const {
+    return ;
+}
+
+void Account::makeDeposit(int deposit) {
+    return ;
+}
+
+bool Account::makeWithdrawal(int withdrawal) {
+    return (0);
+}
